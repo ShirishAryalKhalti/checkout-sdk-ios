2df8ed85a5ec1b403a21c7f7af44391712086faf
bikash giri <bikashgiri17@gmail.com>
2024-05-30 17:32:54 +0545

    Add CheckOutStoryboard and add initialviewcontroller

---
+++ Model/Environment.swift
@@ -0,0 +1,8 @@
+//
+//  Environment.swift
+//  KhaltiCheckout
+//
+//  Created by Mac on 5/30/24.
+//
+
+import Foundation
---
+++ Model/PaymentResult.swift
@@ -0,0 +1,8 @@
+//
+//  PaymentResult.swift
+//  KhaltiCheckout
+//
+//  Created by Mac on 5/30/24.
+//
+
+import Foundation
