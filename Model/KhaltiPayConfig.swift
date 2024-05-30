468cc9c4e09ca32692cf9aa55b8d36b403236856
bikash giri <bikashgiri17@gmail.com>
2024-05-30 16:50:33 +0545

    Add KhaltiPayconfig in model directory

--- Model/KhaltiPayConfig.swift
+++ Model/KhaltiPayConfig.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+
+@objc public class KhaltiPayConfig: NSObject {
+    private var publicKey:String
+    private var productId:String
+    
+    private var pIdx:String
+    private var openInKhalti:bool
+    
+    @objc public init(publicKey:String,  productId:String,pIdx:String,openInKhalti ?= false) {
+        self.publicKey = publicKey
+        self.productId = productId
+        self.openInKhalti = openInKhalti
+        self.pIdx = pidx
+       }
+    
+
+    @objc public func getPidx() -> String {
+        return self.productId
+    }
+    @objc public func getPublicKey() -> String {
+        return self.publicKey
+    }
+    @objc public func getOpenInKhalti() -> String {
+        return self.openInKhalti
+    }
+    @objc public func productId() -> String {
+        return self.productId
+    }
+
+}
+
