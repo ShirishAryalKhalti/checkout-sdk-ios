ca8aca7ad6c49afd4153fc917b36126ca7e4ff38
bikash giri <bikashgiri17@gmail.com>
2024-05-30 15:39:19 +0545

    Add KhaltiCheckoutWebViewController

---
+++ KhaltiCheckout/Classes/KhaltiCheckoutWebViewController.swift
@@ -0,0 +1,61 @@
+
+//
+//  KhaltiWebViewController.swift
+//  Khalti
+//
+//  Created by Mac on 2/20/24.
+//
+
+import UIKit
+import WebKit
+
+class KhaltiCheckOutWebViewController: UIViewController,WKNavigationDelegate, WKUIDelegate {
+    var wkWebView: WKWebView = WKWebView()
+    var request:URLRequest?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        wkWebView.backgroundColor = UIColor.lightGray
+        wkWebView.frame = view.bounds
+        wkWebView.center = view.center
+        wkWebView.isOpaque = false
+        
+        // Initialize WKWebView with a configuration
+          let configuration = WKWebViewConfiguration()
+          wkWebView = WKWebView(frame: .zero, configuration: configuration)
+          
+          wkWebView.backgroundColor = UIColor.lightGray
+          wkWebView.translatesAutoresizingMaskIntoConstraints = false
+          view.addSubview(wkWebView)
+          
+          // Adding observers (uncomment if needed)
+          // wkWebView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+          // wkWebView.addObserver(self, forKeyPath: "title", options: .new, context: nil)
+          
+          NSLayoutConstraint.activate([
+              wkWebView.topAnchor.constraint(equalTo: view.topAnchor),
+              wkWebView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+              wkWebView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+              wkWebView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+          ])
+        let url = URL(string: "https://khalti.com")
+        request = URLRequest(url: url!)
+        loadRequest()
+        // Do any additional setup after loading the view.
+    }
+    
+    func loadRequest() {
+        // To clear Cache of WkWebView
+        let dataStore = WKWebsiteDataStore.default()
+        dataStore.fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            dataStore.removeData(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(), for: records, completionHandler: {
+                if let myRequest = self.request {
+                    self.wkWebView.load(myRequest)
+                    self.wkWebView.navigationDelegate = self
+                    self.wkWebView.uiDelegate = self
+                }
+            })
+        }
+    }
+
+}
