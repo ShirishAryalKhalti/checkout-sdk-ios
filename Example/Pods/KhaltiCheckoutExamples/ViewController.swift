67ed2e7ea66e008697f8ba8dfee452182ef19241
bikash giri <bikashgiri17@gmail.com>
2024-06-06 11:48:13 +0545

    Add button and action in view controller

--- Example/Pods/KhaltiCheckoutExamples/ViewController.swift
+++ Example/Pods/KhaltiCheckoutExamples/ViewController.swift
@@ -6,13 +6,35 @@
 //
 
 import UIKit
+import KhaltiCheckout
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create the button
+               let button = UIButton(type: .system)
+               button.setTitle("Click Me", for: .normal)
+               button.backgroundColor = .systemBlue
+               button.setTitleColor(.white, for: .normal)
+               button.translatesAutoresizingMaskIntoConstraints = false // Enable Auto Layout
+
+               // Add the button to the view
+               view.addSubview(button)
+
+               // Set up Auto Layout constraints
+               NSLayoutConstraint.activate([
+                   button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                   button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                   button.widthAnchor.constraint(equalToConstant: 200),
+                   button.heightAnchor.constraint(equalToConstant: 50)
+               ])
+        
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
+    
+
 
 
 }
