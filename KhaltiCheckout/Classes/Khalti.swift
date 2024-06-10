fd832636988f5b8a5151ec9f6e8ad15eb71a7a08
bikash giri <bikashgiri17@gmail.com>
2024-06-10 17:39:51 +0545

    Add copyWith method to copy properties of instance

--- KhaltiCheckout/Classes/Khalti.swift
+++ KhaltiCheckout/Classes/Khalti.swift
@@ -12,17 +12,43 @@ public typealias OnReturn = (Khalti) -> ()
 
 
 public class Khalti{
-    private var config:KhaltiPayConfig
-    private var onPaymentResult: OnPaymentResult
-    private var onMessage: OnMessageResult
-    private var onReturn:OnReturn
+    public var config:KhaltiPayConfig
+     public var onPaymentResult: OnPaymentResult
+    public var onMessage: OnMessageResult
+    public var onReturn:OnReturn
     
-     public init(config: KhaltiPayConfig,onPaymentResult: @escaping OnPaymentResult,onMessage:@escaping OnMessageResult,onReturn:@escaping OnReturn) {
+    
+    public init(config: KhaltiPayConfig,onPaymentResult: @escaping OnPaymentResult,onMessage:@escaping OnMessageResult,onReturn:@escaping OnReturn) {
         self.config = config
         self.onPaymentResult = onPaymentResult
         self.onMessage = onMessage
         self.onReturn = onReturn
+        KhaltiGlobal.setKhaltiPayconfig(config: self.config)
     }
     
+    
+    func copyWith( config: KhaltiPayConfig? = nil, onPaymentResult: OnPaymentResult? = nil, onMessage: OnMessageResult? = nil,onReturn:OnReturn? = nil) -> Khalti{
+        return Khalti(
+            config: config ?? self.config,
+            onPaymentResult: onPaymentResult ?? self.onPaymentResult,
+            onMessage: onMessage ?? self.onMessage,
+            onReturn: onReturn ?? self.onReturn
+        )
+    }
+    
+    func getConfig() -> KhaltiPayConfig{
+        return self.config
+    }
+    
+    
+    
+    
+    public func open(viewController:UIViewController){
+        let vc = KhaltiPaymentViewController()
+        //            vc.modalPresentationStyle = .fullScreen
+        
+        viewController.present(vc, animated:true)
+        
+    }
     
 }
--- KhaltiCheckout/Classes/Model/KhaltiPayConfig.swift
+++ KhaltiCheckout/Classes/Model/KhaltiPayConfig.swift
@@ -9,31 +9,46 @@ import Foundation
 
 
 public class KhaltiPayConfig: NSObject {
-    private var publicKey:String
-    private var pIdx:String
-    private var openInKhalti:Bool? = false
-    private var environment:Environment = Environment.TEST
+    public var publicKey:String
+    public var pIdx:String
+    public var openInKhalti:Bool? = false
+    public var environment:Environment = Environment.TEST
     
-      public init(publicKey:String,pIdx:String,openInKhalti:Bool? = false,environment:Environment) {
+    public init(publicKey:String,pIdx:String,openInKhalti:Bool? = false,environment:Environment) {
         self.publicKey = publicKey
         self.pIdx = pIdx
         self.openInKhalti = openInKhalti
         self.environment = environment
     }
     
-
-      func getPidx() -> String {
+    public func copyWith( publicKey: String? = nil,pIdx:String? = nil,openInKhalti:Bool? = false,environmnet:Environment? = nil) -> KhaltiPayConfig{
+        return KhaltiPayConfig(
+            publicKey: publicKey ?? self.publicKey,
+            pIdx: pIdx ?? self.pIdx,
+            openInKhalti: openInKhalti ?? self.openInKhalti,
+            environment : environment ?? self.environment
+        )
+    }
+    
+    
+    
+    
+    
+    func getPidx() -> String {
         return self.pIdx
     }
-      func getPublicKey() -> String {
+    func getPublicKey() -> String {
         return self.publicKey
     }
     func getOpenInKhalti() -> Bool{
         return self.openInKhalti!
     }
-      func getEnvironment() -> Environment{
+    func getEnvironment() -> Environment{
         return self.environment
     }
-
+    func isProd() -> Bool{
+        return self.environment == Environment.PROD
+    }
+    
 }
 
