fda6843f693b9caa7d9e6969bce948bbd15ea067
bikash giri <bikashgiri17@gmail.com>
2024-06-11 18:27:38 +0545

    Add method to appendBaseUrl

--- KhaltiCheckout/Classes/Resources/Url.swift
+++ KhaltiCheckout/Classes/Resources/Url.swift
@@ -11,4 +11,11 @@ enum Url:String {
     case BASE_KHALTI_URL_STAGING = "https://dev.khalti.com/api/v2/"
     case BASE_PAYMENT_URL_PROD = "https://pay.khalti.com/"
     case BASE_PAYMENT_URL_STAGING = "https://test-pay.khalti.com/"
+    
+    case PAYMENT_DETAIL = "epayment/detail/"
+    case LOOKUP_SDK = "epayment/lookup-sdk/"
+    
+    func appendBaseUrl(url:String) -> String{
+        return self.rawValue + url
+    }
 }
