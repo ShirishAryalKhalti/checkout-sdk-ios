a32b73be348c653d9d3cc4386d819d43b8feed24
bikash giri <bikashgiri17@gmail.com>
2024-06-11 18:28:00 +0545

    Add model

--- KhaltiCheckout/Classes/Model/PaymentDetailModel.swift
+++ KhaltiCheckout/Classes/Model/PaymentDetailModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct PaymentDetailModel: Codable {
+    let id: Int
+    let name: String
+    let email: String
+    let profilePictureURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case email
+        case profilePictureURL = "profile_picture_url"
+    }
+}
+
