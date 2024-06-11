15e7c4d31deec6b3330c381f840567ce93fda027
bikash giri <bikashgiri17@gmail.com>
2024-06-11 18:27:24 +0545

    Add fetchDetail method for network request

--- Service/KhaltiApi.swift
+++ Service/KhaltiApi.swift
@@ -6,3 +6,120 @@
 //
 
 import Foundation
+
+class KhaltiAPI {
+    
+//    static let shared = KhaltiAPI()
+//    static let logMessage:Bool = Khalti.shared.debugLog
+    
+    private func createRequest(for url:URL) -> URLRequest {
+        
+        var request = URLRequest(url: url)
+//        request.httpBody = ["":""]
+        request.setValue(Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String, forHTTPHeaderField: "checkout-version")
+        request.setValue("iOS", forHTTPHeaderField: "checkout-source")
+        request.setValue(UIDevice.current.model, forHTTPHeaderField: "checkout-device-model")
+        request.setValue(UIDevice.current.identifierForVendor?.uuidString ?? "", forHTTPHeaderField: "checkout-device-id")
+        request.setValue(UIDevice.current.systemVersion, forHTTPHeaderField: "checkout-ios-version")
+        return request
+    }
+    
+    func fetchDetail( onCompletion: @escaping ((PaymentDetailModel)->()), onError: @escaping ((String)->())) {
+        let config = KhaltiGlobal.khaltiConfig
+        let urlEnv = (config?.isProd() ?? false) ? Url.BASE_KHALTI_URL_PROD: Url.BASE_KHALTI_URL_STAGING
+        let url = urlEnv.appendBaseUrl(url:Url.PAYMENT_DETAIL.rawValue)
+        
+
+        guard let url = URL(string: url) else {
+            onError("Error on parsing Url")
+            return
+        }
+        
+        let configuration = URLSessionConfiguration.ephemeral
+        let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
+        let request = self.createRequest(for: url)
+       
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                onError(error.localizedDescription)
+                return
+            }
+            
+            print("===========================================================")
+            print("Status: \(request), Response for: \(url)")
+            print("===========================================================")
+          
+
+            
+            guard let data = data else {
+//                onError(NetworkError.noData.rawValue)
+                return
+            }
+            
+            
+            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+//                onError(NetworkError.noResponse.rawValue)
+                return
+            }
+  
+                            print("Status: \(response.statusCode), Response for: \(url)")
+                            print("===========================================================")
+            print("\(response)")
+                        print("===========================================================")
+
+            
+            do {
+                let user = try JSONDecoder().decode(PaymentDetailModel.self, from: data)
+                onCompletion(user)
+            } catch {
+//                onError(NetworkError.parseError.rawValue)
+            }
+        }
+        
+        task.resume()
+    }
+    
+//    func epaymentLookUp(onCompletion: @escaping (([String:Any])->()), onError: @escaping ((String)->())) {
+//        
+//        let urlString:String = KhaltiAPIUrl.cardTerms.rawValue
+//        guard let url = URL(string: urlString) else {
+//            onError(ErrorMessage.invalidUrl.rawValue)
+//            return
+//        }
+//        
+//        let configuration = URLSessionConfiguration.ephemeral
+//        let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
+//        let request = self.createRequest(for: url)
+//        let task = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+//            
+//            guard let data = data else {
+//                onError((error?.localizedDescription)!)
+//                return
+//            }
+//            
+//            guard let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) else {
+////                onError(ErrorMessage.parse.rawValue)
+//                return
+//            }
+//            guard let responsee = response as? HTTPURLResponse else {
+////                onError(ErrorMessage.noresponse.rawValue)
+//                return
+//            }
+//            
+////            if KhaltiAPI.logMessage {
+////                print("Status: \(responsee.statusCode), Response for: \(url)")
+////                print("===========================================================")
+////                print("\(json)")
+////                print("===========================================================")
+////            }
+//            
+//            if let value = json as? [String:Any] {
+//                onCompletion(value)
+//            } else {
+//                onError(error?.localizedDescription ?? ErrorMessage.tryAgain.rawValue)
+//            }
+//        }
+//        task.resume()
+//    }
+    
+}
