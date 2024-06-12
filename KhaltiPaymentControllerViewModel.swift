7e80a7c2703ad07a3ab1edee2624192b094d2ca1
bikash giri <bikashgiri17@gmail.com>
2024-06-12 18:38:23 +0545

    Parse environment and pass url to service

--- KhaltiPaymentControllerViewModel.swift
+++ KhaltiPaymentControllerViewModel.swift
@@ -7,14 +7,17 @@
 
 import Foundation
 class KhaltiPaymentControllerViewModel {
-    
+    let khalti = KhaltiGlobal.khalti
     let service = KhaltiAPI()
     
     func getPaymentDetail(onCompletion: @escaping ((PaymentDetailModel)->()), onError: @escaping ((String)->())){
+        let baseUrl = isProd() ? Url.BASE_KHALTI_URL_PROD: Url.BASE_PAYMENT_URL_STAGING
+        
+        let url = baseUrl.appendUrl(url: Url.PAYMENT_DETAIL)
         if let pIdx = KhaltiGlobal.khaltiConfig?.pIdx {
             var params = [String:String]()
             params["pidx"] = pIdx
-            service.fetchDetail(params: params, onCompletion: {(response) in
+            service.fetchDetail(url:url,params: params, onCompletion: {(response) in
                 onCompletion(response)
             }, onError: {(error) in
                 onError(error)
@@ -24,5 +27,14 @@ class KhaltiPaymentControllerViewModel {
         }
         
         
+    }
+    
+    func verifyPaymentStatus(onCompletion:@escaping((PaymentDetailModel)->()),onError: @escaping ((String)->())){
+        
+        
+    }
+    
+    private func isProd() ->Bool{
+        khalti?.config.isProd() ?? false
     }
 }
