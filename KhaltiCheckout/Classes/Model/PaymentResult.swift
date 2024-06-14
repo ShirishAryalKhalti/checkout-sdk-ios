56274c56c5852b2c5d11cf774c09f06e63e43814
bikash giri <bikashgiri17@gmail.com>
2024-06-14 18:30:04 +0545

    Change properties of paymentresult

--- KhaltiCheckout/Classes/Model/PaymentResult.swift
+++ KhaltiCheckout/Classes/Model/PaymentResult.swift
@@ -6,39 +6,15 @@
 //
 import Foundation
 
-@objc public class PaymentResult: NSObject {
-    private var status:String
-    private var message:String
-    private var returnUrl:String?
-    private var amount:Double
-    private var openInKhalti:Bool = false
-    private var onPaymentResult:PaymentPayLoad?
+ public class PaymentResult: NSObject {
+    private var status:String?
+    private var payload:PaymentLoadModel?
+     private var khalti:Khalti?
     
-    @objc public init(status:String, message:String,payload:String,amount:Double = 0, openInKhalti:Bool = false,returnUrl:String,onPaymentResult:PaymentPayLoad?) {
+     init(status:String, message:String,payload:PaymentLoadModel,khalti:Khalti) {
         self.status = status
-        self.message = message
-        self.returnUrl = returnUrl
-        self.amount = amount
-        self.onPaymentResult = onPaymentResult
-        self.openInKhalti = openInKhalti
+        self.payload = payload
+         self.khalti = khalti
         
     }
 }
-    
-    @objc public class PaymentPayLoad: NSObject {
-        private var pidx:String
-        private var totalAmount:Double
-        private var status:String
-        private var transactionId:String
-        private var fee:Double
-        private var refunded:Bool = true
-        
-        @objc public init(pidx:String, totalAmount:Double, status:String,transactionId:String,fee:Double = 0, refunded: Bool = false) {
-            self.pidx = pidx
-            self.totalAmount = totalAmount
-            self.status = status
-            self.transactionId = transactionId
-            self.fee = fee
-            self.refunded = refunded
-        }
-    }
