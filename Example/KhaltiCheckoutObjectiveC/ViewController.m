fb2a383096975233702469225c1cc6f967189c4c
bikash giri <bikashgiri17@gmail.com>
2024-07-08 18:00:29 +0545

    Add button in viewcontroller

--- Example/KhaltiCheckoutObjectiveC/ViewController.m
+++ Example/KhaltiCheckoutObjectiveC/ViewController.m
@@ -21,16 +21,38 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self setupKhalti];
+    
+    // Create the button
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"Pay" forState:UIControlStateNormal];
+    button.backgroundColor = [UIColor lightGrayColor];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    button.frame = CGRectMake(100, 100, 200, 50);
+    
+    // Add target for button action
+    [button addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Add button to the view
+    [self.view addSubview:button];
+    
+    // Set button constraints
+    button.translatesAutoresizingMaskIntoConstraints = NO;
+    [NSLayoutConstraint activateConstraints:@[
+        [button.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [button.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor],
+        [button.widthAnchor constraintEqualToConstant:200],
+        [button.heightAnchor constraintEqualToConstant:50]
+    ]];
 }
 
 - (void)setupKhalti {
     // Khalti Configuration
-
     
-    KhaltiPayConfig *config = [[KhaltiPayConfig alloc] initWithPublicKey:@"4aa1b684f4de4860968552558fc8487d" pIdx:@"tehsEsSbeEeLD5ECPrWDk2" openInKhalti:false environment:EnvironmentTEST];
+    
+    KhaltiPayConfig *config = [[KhaltiPayConfig alloc] initWithPublicKey:@"4aa1b684f4de4860968552558fc8487d" pIdx:@"ycMpVrifXFWGT3TBgsVW62" openInKhalti:false environment:EnvironmentTEST];
     
     __weak typeof(self) weakSelf = self;
-       
+    
     
     // Initialize Khalti
     self.khalti = [[Khalti alloc] initWithConfig:config
@@ -38,7 +60,7 @@
         __strong typeof(weakSelf) strongSelf = weakSelf;
         NSLog(@"Demo | onPaymentResult: %@", paymentResult);
         [khalti close];
-    
+        
         
         [strongSelf showSuccessAlertWithTitle:@"Success" message:paymentResult.getMessage ?: @"Success"];
         
@@ -60,6 +82,12 @@
     } onReturn:^(Khalti *khalti) {
         // Called when payment is success
     }];
+}
+
+// Action method for button tap
+- (void)buttonTapped:(UIButton *)sender {
+    
+    [self.khalti openWithViewController:self];
 }
 
 - (void)showSuccessAlertWithTitle:(NSString *)title message:(NSString *)message {
